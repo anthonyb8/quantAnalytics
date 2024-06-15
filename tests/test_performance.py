48c22b1f5c526ae54dfc2b056ea881a9827d388a
anthony baxter <anthony_baxter@student.uml.edu>
2024-06-15 13:32:45 -0400

    MOD: Modified perforamcne toincldue returns and not longer include trade calculations.

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -1,239 +1,123 @@
 import unittest
 import numpy as np
 import pandas as pd
+from unittest.mock import Mock, patch
 
 from quantAnalytics.performance import PerformanceStatistics
 
 # TODO: edge cases
-class TestPerformancStatistics(unittest.TestCase):    
+
+class TestReturns(unittest.TestCase):    
     def setUp(self):
-        # Sample equity curve and trade log for testing
         self.equity_curve = np.array([100, 105, 103, 110, 108])
-        self.benchmark_equity_curve = np.array([100, 103, 102, 106, 108])
-        
-        self.trade_log = pd.DataFrame({
-            'pnl': [20, -10, 15, -5, 30, -20],
-            'gain/loss': [0.10, -0.05, 0.075, -0.25, 0.15, -0.010]
-        })
+        self.daily_returns = np.array([0.001, -0.002, 0.003, 0.002, -0.001])
     
-    # Net Profit
+    # Basic Validation
+    def test_simple_returns(self):
+        # expected
+        expected_returns = np.array([0.05, -0.019047, 0.067961, -0.018181])
+        
+        # test
+        actual =PerformanceStatistics.simple_returns(self.equity_curve)
+
+        # validate
+        np.testing.assert_allclose(actual, expected_returns, atol=1e-5)
+    
+    def test_log_returns(self):
+        # expected 
+        expected_returns = np.array([ 0.04879016 ,-0.01923136  ,0.06575138 ,-0.01834914])
+
+        # test
+        actual =PerformanceStatistics.log_returns(self.equity_curve)
+
+        # validate
+        np.testing.assert_allclose(actual, expected_returns, atol=1e-5)
+
+    def test_cumulative_return(self):
+        # expected
+        expected_cumulative_returns = np.array([0.05, 0.03, 0.10, 0.08])
+        
+        # test
+        cumulative_returns =PerformanceStatistics.cumulative_returns(self.equity_curve)
+        
+        # validate
+        np.testing.assert_array_almost_equal(cumulative_returns, expected_cumulative_returns, decimal=4)
+    
+    def test_total_return(self):
+        # expected
+        expected_total_return = 0.08 
+        
+        # test
+        total_return =PerformanceStatistics.total_return(self.equity_curve)
+        
+        # validate
+        self.assertEqual(total_return, expected_total_return)
+
+    def test_annualize_returns(self):
+        # expected
+        compounded_growth = (1+self.daily_returns).prod()
+        n_periods = self.daily_returns.shape[0]
+        expected_annualized_return = compounded_growth ** (252 / n_periods) - 1
+
+        # test
+        actual =PerformanceStatistics.annualize_returns(self.daily_returns)
+
+        # validate
+        self.assertAlmostEqual(actual, expected_annualized_return)
+
     def test_net_profit(self):
-        # Expected result based on the sample trade log
-        expected_net_profit = 30  
-        
-        # test
-        net_profit = PerformanceStatistics.net_profit(self.trade_log)
-        
-        # validate
-        self.assertEqual(net_profit, expected_net_profit)
+        # expected
+        expected_net_profit = self.equity_curve[-1] - self.equity_curve[0]
 
-    def test_net_profit_type_check(self):        
-        # Test with incorrect type (should raise an error or handle gracefully)
+        # test
+        actual =PerformanceStatistics.net_profit(self.equity_curve)
+
+        # validate
+        self.assertAlmostEqual(actual, expected_net_profit)
+
+    # Type Constraint
+    def test_simple_returns_type_error(self):
         with self.assertRaises(TypeError):
-            PerformanceStatistics.net_profit([10, -5, 15])
+           PerformanceStatistics.simple_returns([1,2,3,4,5])
 
-        # Test with missing column
-        with self.assertRaises(ValueError):
-            PerformanceStatistics.net_profit(pd.DataFrame())
-
-    def test_net_profit_null_handling(self):
-        # Test with empty DataFrame
-        trade_log = pd.DataFrame({'pnl': []})
-        
-        # validate
-        self.assertEqual(PerformanceStatistics.net_profit(trade_log), 0)
-
-    # Total Trades
-    def test_total_trades(self):
-        expected_total_trades = 6
-        # test
-        total_trades = PerformanceStatistics.total_trades(self.trade_log)
-        # validate
-        self.assertEqual(total_trades, expected_total_trades)
-
-    # Total Winning Trades
-    def test_total_winning_trades(self):
-        expected_winning_trades = 3
-        # test
-        total_winning_trades = PerformanceStatistics.total_winning_trades(self.trade_log)
-        # validate
-        self.assertEqual(total_winning_trades, expected_winning_trades)
-
-    def test_total_winning_trades_type_check(self):        
-        # Test with incorrect type (should raise an error or handle gracefully)
+    def test_log_returns_type_error(self):
         with self.assertRaises(TypeError):
-            PerformanceStatistics.total_winning_trades([10, -5, 15])
-
-        # Test with missing column
-        with self.assertRaises(ValueError):
-            PerformanceStatistics.total_winning_trades(pd.DataFrame())
-
-    def test_total_winning_trades_null_handling(self):
-        # Test with empty DataFrame
-        trade_log = pd.DataFrame({'pnl': []})
-        # validate
-        self.assertEqual(PerformanceStatistics.total_winning_trades(trade_log), 0)
+           PerformanceStatistics.log_returns([1,2,3,4,5])
     
-    # Total Losing Trades
-    def test_total_losing_trades(self):
-        expected_losing_trades = 3
-        # test
-        total_losing_trades = PerformanceStatistics.total_losing_trades(self.trade_log)
-        # validate
-        self.assertEqual(total_losing_trades, expected_losing_trades)
-
-    def test_total_losing_trades_type_check(self):        
-        # Test with incorrect type (should raise an error or handle gracefully)
+    def test_cumulative_return_type_error(self):
         with self.assertRaises(TypeError):
-            PerformanceStatistics.total_losing_trades([10, -5, 15])
-
-        # Test with missing column
-        with self.assertRaises(ValueError):
-            PerformanceStatistics.total_losing_trades(pd.DataFrame())
-
-    def test_total_losing_trades_null_handling(self):
-        # Test with empty DataFrame
-        trade_log = pd.DataFrame({'pnl': []})
-        self.assertEqual(PerformanceStatistics.total_losing_trades(trade_log), 0)
+           PerformanceStatistics.cumulative_returns([10, -5, 15])
     
-    # Total Avg Win Percent
-    def test_avg_win_return_rate(self):
-        expected_avg_win = 0.108333  # Based on the provided gain/loss values
-        # test
-        avg_win_return_rate = PerformanceStatistics.avg_win_return_rate(self.trade_log)
-        # validate
-        self.assertAlmostEqual(avg_win_return_rate, expected_avg_win, places=4)
-
-    def test_avg_win_return_rate_type_check(self):        
-        # Test with incorrect type (should raise an error or handle gracefully)
+    def test_total_return_type_error(self):
         with self.assertRaises(TypeError):
-            PerformanceStatistics.avg_win_return_rate([10, -5, 15])
-
-        # Test with missing column
-        with self.assertRaises(ValueError):
-            PerformanceStatistics.avg_win_return_rate(pd.DataFrame())
-
-    def test_avg_win_return_rate_null_handling(self):
-        # Test with empty DataFrame
-        trade_log = pd.DataFrame({'pnl': []})
-        # validate
-        self.assertEqual(PerformanceStatistics.avg_win_return_rate(trade_log), 0)
+           PerformanceStatistics.total_return([10, -5, 15])
     
-    # Total avg_loss_return_rate
-    def test_avg_loss_return_rate(self):
-        expected_avg_loss = -0.10333  # Based on the provided gain/loss values
-        # test
-        avg_loss_return_rate = PerformanceStatistics.avg_loss_return_rate(self.trade_log)
-        # validate
-        self.assertAlmostEqual(avg_loss_return_rate, expected_avg_loss, places=4)
-
-    def test_avg_loss_return_rate_type_check(self):        
-        # Test with incorrect type (should raise an error or handle gracefully)
+    def test_annualize_returns_type_error(self):
         with self.assertRaises(TypeError):
-            PerformanceStatistics.avg_loss_return_rate([10, -5, 15])
+           PerformanceStatistics.annualize_returns([1,2,3,4,5])
+    
+    # Error Handling
+    def test_simple_returns_calculation_error(self):
+        with self.assertRaises(Exception):
+           PerformanceStatistics.simple_returns(np.array([1,2,3,'4']))
+    
+    def test_log_returns_calculation_error(self):
+        with self.assertRaises(Exception):
+           PerformanceStatistics.log_returns(np.array([1,2,3,'4']))
 
-        # Test with missing column
-        with self.assertRaises(ValueError):
-            PerformanceStatistics.avg_loss_return_rate(pd.DataFrame())
+    def test_cumulative_returns_calculation_error(self):
+        with self.assertRaises(Exception):
+           PerformanceStatistics.cumulative_returns(np.array([1,2,3,'4']))
 
-    def test_avg_loss_return_rate_null_handling(self):
-        # Test with empty DataFrame
-        trade_log = pd.DataFrame({'pnl': []})
-        # validate
-        self.assertEqual(PerformanceStatistics.avg_loss_return_rate(trade_log), 0)
-
-    # Percent Profitable
-    def test_profitability_ratio(self):
-        expected_profitability_ratio = 0.50  # 3 winning trades out of 6 total trades
-        # test
-        profitability_ratio = PerformanceStatistics.profitability_ratio(self.trade_log)
-        # validate
-        self.assertEqual(profitability_ratio, expected_profitability_ratio)
-
-    def test_profitability_ratio_type_check(self):        
-        # Test with incorrect type (should raise an error or handle gracefully)
-        with self.assertRaises(TypeError):
-            PerformanceStatistics.profitability_ratio([10, -5, 15])
-
-        # Test with missing column
-        with self.assertRaises(ValueError):
-            PerformanceStatistics.profitability_ratio(pd.DataFrame())
-
-    def test_profitability_ratio_null_handling(self):
-        # Test with empty DataFrame
-        trade_log = pd.DataFrame({'pnl': []})
-        # valdiate
-        self.assertEqual(PerformanceStatistics.profitability_ratio(trade_log), 0)
-
-    # Avg Trade Profit
-    def test_avg_trade_profit(self):
-        expected_avg_trade_profit = 5  # (20-10+15-5+30-20) / 6
-        # test
-        average_trade_profit = PerformanceStatistics.avg_trade_profit(self.trade_log)
-        # validate
-        self.assertEqual(average_trade_profit, expected_avg_trade_profit)
-
-    def test_avg_trade_profit_type_check(self):        
-        # Test with incorrect type (should raise an error or handle gracefully)
-        with self.assertRaises(TypeError):
-            PerformanceStatistics.avg_trade_profit([10, -5, 15])
-
-        # Test with missing column
-        with self.assertRaises(ValueError):
-            PerformanceStatistics.avg_trade_profit(pd.DataFrame())
-
-    def test_avg_trade_profit_null_handling(self):
-        # Test with empty DataFrame
-        trade_log = pd.DataFrame({'pnl': []})
-        # validate
-        self.assertEqual(PerformanceStatistics.avg_trade_profit(trade_log), 0)
-
-    # Profit Factor
-    def test_profit_factor(self):
-        expected_profit_factor = 1.8571 # (20+15+30) / abs(-10-5-20)
-        # test
-        profit_factor = PerformanceStatistics.profit_factor(self.trade_log)
-        # validate
-        self.assertEqual(profit_factor, expected_profit_factor)
-
-    def test_profit_factor_type_check(self):        
-        # Test with incorrect type (should raise an error or handle gracefully)
-        with self.assertRaises(TypeError):
-            PerformanceStatistics.profit_factor([10, -5, 15])
-
-        # Test with missing column
-        with self.assertRaises(ValueError):
-            PerformanceStatistics.profit_factor(pd.DataFrame())
-
-    def test_profit_factor_null_handling(self):
-        # Test with empty DataFrame
-        trade_log = pd.DataFrame({'pnl': []})
-        # validate
-        self.assertEqual(PerformanceStatistics.profit_factor(trade_log), 0)
-
-    # Profit & Loss Ratio
-    def test_profit_and_loss_ratio(self):
-        expected_pnl_ratio = 1.8571  # abs(mean([20,15,30]) / mean([-10,-5,-20]))
-        # test
-        profit_and_loss_ratio = PerformanceStatistics.profit_and_loss_ratio(self.trade_log)
-        # validate
-        self.assertEqual(profit_and_loss_ratio, expected_pnl_ratio)
-
-    def test_profit_and_loss_ratio_type_check(self):        
-        # Test with incorrect type (should raise an error or handle gracefully)
-        with self.assertRaises(TypeError):
-            PerformanceStatistics.profit_and_loss_ratio([10, -5, 15])
-
-        # Test with missing column
-        with self.assertRaises(ValueError):
-            PerformanceStatistics.profit_and_loss_ratio(pd.DataFrame())
-
-    def test_profit_and_loss_ratio_null_handling(self):
-        # Test with empty DataFrame
-        trade_log = pd.DataFrame({'pnl': []})
-        # validate
-        self.assertEqual(PerformanceStatistics.profit_and_loss_ratio(trade_log), 0)
-
+    def test_total_returns_calculation_error(self):
+        with self.assertRaises(Exception):
+           PerformanceStatistics.total_return(np.array([1,2,3,'4']))
+    
+    def test_annualize_returns_calculation_error(self):
+        with self.assertRaises(Exception):
+           PerformanceStatistics.annualize_returns(np.array([1,2,3,'4']))
+        
+    # Edge Case
 
 if __name__ == "__main__":
     unittest.main()
