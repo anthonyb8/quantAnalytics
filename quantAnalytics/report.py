0094c44dfd4f3060471bbb316610cd6dac43566b
anthony baxter <anthony_baxter@student.uml.edu>
2024-09-16 14:20:11 -0400

    Implmented builder structure on report.

--- quantAnalytics/report.py
+++ quantAnalytics/report.py
@@ -15,45 +15,22 @@ class Header(Enum):
     H6 = "h6"
 
 
-class ReportGenerator:
+class ComponentBuilder:
+    def __init__(self):
+        self._content = ""
 
-    def __init__(self, file_name: str, output_directory: str = "report", css_path: str = "") -> None:
-        # Create the report directory if it doesn't exist
-        if not os.path.exists(output_directory):
-            os.makedirs(output_directory)
-
-        # Set paths
-        self.output_directory = output_directory
-        self.file_path = os.path.join(output_directory, file_name)
-        self._html_content = ""
-
-        if not css_path:
-            with resources.as_file(resources.files("quantAnalytics") / "styles.css") as css_file:
-                self.css_path = str(css_file)
-
-            # If no custom css_path is provided, load the default styles.css
-            # with importlib.resources.files("quantAnalytics", "styles.css") as css_file:
-            #     self.css_path = str(css_file)
-        else:
-            self.css_path = css_path
-
-        # HTML header
-        self._init_report()
-
-    def _init_report(self) -> None:
-        """Create HTML header."""
-        self._html_content += f"""<!DOCTYPE html>\n<html>\n<head>\n    <title>Backtest Report</title>\n    <link rel="stylesheet" href="{self.css_path}">\n</head>\n<body>"""
-
-    def add_header(self, text: str, header: Header, css_class: Optional[str] = None) -> None:
+    def add_header(self, text: str, header: Header, css_class: Optional[str] = None) -> "ComponentBuilder":
         """Add a html header block."""
         header_str = header.value
 
         if css_class:
-            self._html_content += f"""\n<{header_str} class="{css_class}">{text}</{header_str}>"""
+            self._content += f"""\n<{header_str} class="{css_class}">{text}</{header_str}>"""
         else:
-            self._html_content += f"""\n<{header_str}>{text}</{header_str}>"""
+            self._content += f"""\n<{header_str}>{text}</{header_str}>"""
 
-    def add_paragraph(self, text: str, css_class: Optional[str] = None) -> None:
+        return self
+
+    def add_paragraph(self, text: str, css_class: Optional[str] = None) -> "ComponentBuilder":
         """
         Adds a paragraph of text to the report with an optional CSS class.
 
@@ -66,12 +43,15 @@ class ReportGenerator:
 
         if css_class:
             # Add the class to the paragraph if provided
-            self._html_content += f'<p class="{css_class}">{text}</p>\n'
+            self._content += f'<p class="{css_class}">{text}</p>\n'
         else:
             # Add a plain paragraph if no class is provided
-            self._html_content += f"<p>{text}</p>\n"
+            self._content += f"<p>{text}</p>\n"
+        return self
 
-    def add_unorderedlist_dict(self, items: Dict, bold_key: bool = True, css_class: Optional[str] = None) -> None:
+    def add_unorderedlist_dict(
+        self, items: Dict, bold_key: bool = True, css_class: Optional[str] = None
+    ) -> "ComponentBuilder":
         """
         Adds a bullet-point list to the report summarizing key-value pairs, with an optional CSS class.
 
@@ -85,20 +65,22 @@ class ReportGenerator:
 
         # Start the unordered list with a CSS class if provided
         if css_class:
-            self._html_content += f'\n<ul class="{css_class}">'
+            self._content += f'\n<ul class="{css_class}">'
         else:
-            self._html_content += "\n<ul>"
+            self._content += "\n<ul>"
 
         # Iterate over the dictionary items and create list items
         for key, value in items.items():
             if bold_key:
-                self._html_content += f"\n<li><strong>{key}:</strong> {value}</li>"
+                self._content += f"\n<li><strong>{key}:</strong> {value}</li>"
             else:
-                self._html_content += f"\n<li>{key}: {value}</li>"
+                self._content += f"\n<li>{key}: {value}</li>"
 
-        self._html_content += "\n</ul>"
+        self._content += "\n</ul>"
 
-    def add_unorderedlist(self, items: list, css_class: Optional[str] = None) -> None:
+        return self
+
+    def add_unorderedlist(self, items: list, css_class: Optional[str] = None) -> "ComponentBuilder":
         """
         Adds a simple bullet-point list to the report, with an optional CSS class.
 
@@ -111,32 +93,34 @@ class ReportGenerator:
 
         # Start the unordered list with a CSS class if provided
         if css_class:
-            self._html_content += f'\n<ul class="{css_class}">'
+            self._content += f'\n<ul class="{css_class}">'
         else:
-            self._html_content += "\n<ul>"
+            self._content += "\n<ul>"
 
         # Iterate over the list items and create list items
         for item in items:
-            self._html_content += f"\n<li>{item}</li>"
+            self._content += f"\n<li>{item}</li>"
 
-        self._html_content += "\n</ul>"
+        self._content += "\n</ul>"
+        return self
 
-    def add_orderedlist(self, items: List, css_class: Optional[str] = None) -> None:
+    def add_orderedlist(self, items: List, css_class: Optional[str] = None) -> "ComponentBuilder":
         if not isinstance(items, list):
             raise TypeError(f"Expected items to be a list, but got {type(items).__name__}")
 
         if css_class:
             # Add the class to the unordered list if provided
-            self._html_content += f'\n<ol class="{css_class}">'
+            self._content += f'\n<ol class="{css_class}">'
         else:
             # Add a plain unordered list if no class is provided
-            self._html_content += "\n<ol>"
+            self._content += "\n<ol>"
 
         # Iterate over the dictionary items and create list items
         for value in items:
-            self._html_content += f"\n<li>{value}</li>"
+            self._content += f"\n<li>{value}</li>"
 
-        self._html_content += "\n</ol>"
+        self._content += "\n</ol>"
+        return self
 
     def add_table(
         self,
@@ -146,7 +130,7 @@ class ReportGenerator:
         header_css_class: Optional[str] = None,
         table_css_class: Optional[str] = None,
         index: bool = False,
-    ) -> None:
+    ) -> "ComponentBuilder":
         """
         Adds a pandas DataFrame as an HTML table to the report. Optionally includes a title for the DataFrame.
 
@@ -164,20 +148,21 @@ class ReportGenerator:
         if header:
             header_str = header_size.value
             if header_css_class:
-                self._html_content += f"\n<{header_str} class='{header_str}'>{header}</{header_str}>"
+                self._content += f"\n<{header_str} class='{header_str}'>{header}</{header_str}>"
             else:
-                self._html_content += f"\n<{header_str} class='{header_str}'>{header}</{header_str}>"
+                self._content += f"\n<{header_str} class='{header_str}'>{header}</{header_str}>"
 
         # Add the table with a CSS class if provided
         if table_css_class:
-            self._html_content += f'\n<table class="{table_css_class}">'
+            self._content += f'\n<table class="{table_css_class}">'
         else:
-            self._html_content += "\n<table>"
+            self._content += "\n<table>"
 
-        self._html_content += df.to_html(index=index, border=1)
-        self._html_content += "\n</table>"
+        self._content += df.to_html(index=index, border=1)
+        self._content += "\n</table>"
+        return self
 
-    def add_image(self, image_filename: str, css_class: Optional[str] = None) -> None:
+    def add_image(self, image_filename: str, css_class: Optional[str] = None) -> "ComponentBuilder":
         """
         Adds an image to the report with an optional CSS class.
         """
@@ -186,17 +171,73 @@ class ReportGenerator:
 
         # Add the <img> tag with optional CSS class
         if css_class:
-            self._html_content += f'\n<img src="{image_filename}" class="{css_class}" alt="Plot Image"><br>'
+            self._content += f'\n<img src="{image_filename}" class="{css_class}" alt="Plot Image"><br>'
         else:
-            self._html_content += f'\n<img src="{image_filename}" alt="Plot Image"><br>'
+            self._content += f'\n<img src="{image_filename}" alt="Plot Image"><br>'
+        return self
 
-    def complete_report(self) -> None:
+    def add_html_block(self, html: str) -> "ComponentBuilder":
         """
-        Generates both the HTML and PDF versions of the report.
+        Adds a prebuilt HTML block to the report or div. Optionally wraps it in a div.
 
-        Raises:
-        - Exception: If an error occurs during the report generation process.
+        Parameters:
+        - html (str): The raw HTML block to be added.
         """
+        if not isinstance(html, str):
+            raise TypeError(f"Expected prebuilt_html to be a string, but got {type(html).__name__}")
+
+        self._content += f"\n{html}"
+        return self
+
+    def get_content(self) -> str:
+        """Return the generated content."""
+        return self._content
+
+
+class DivBuilder(ComponentBuilder):
+    def __init__(self):
+        super().__init__()
+        self.div_class = ""
+
+    def set_class(self, div_class: str) -> "DivBuilder":
+        """Set the CSS class for the div."""
+        self.div_class = div_class
+        return self
+
+    def build(self) -> str:
+        """Construct the div and return the HTML."""
+
+        div_class_str = f' class="{self.div_class}"' if self.div_class else ""
+
+        wrapped_content = f"""\n<div{div_class_str}> \n{self.get_content().strip()} \n</div>"""
+        return wrapped_content
+
+
+class ReportBuilder(ComponentBuilder):
+    def __init__(self, file_name: str, output_directory: str = "report", css_path: str = ""):
+        super().__init__()
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        self.output_directory = output_directory
+        self.file_path = os.path.join(output_directory, file_name)
+
+        if not css_path:
+            with resources.as_file(resources.files("quantAnalytics") / "styles.css") as css_file:
+                self.css_path = str(css_file)
+        else:
+            self.css_path = css_path
+
+        self._content = f"""<!DOCTYPE html>\n<html>\n<head>\n    <title>Report</title>\n    <link rel="stylesheet" href="{self.css_path}">\n</head>\n<body>"""
+
+    def add_div(self, div_content: str) -> "ReportBuilder":
+        """Add a div block to the report."""
+        self._content += div_content
+        return self
+
+    def build(self) -> None:
+        """Finalize and return the report content."""
+        self._content += f"\n</body>\n</html>"
         try:
             self._generate_html()
             self._generate_pdf()
@@ -210,11 +251,9 @@ class ReportGenerator:
         Side Effect:
         - Writes the complete HTML content to a file, overwriting any existing file with the same name.
         """
-        self._html_content += "\n</body>\n</html>"
-
         try:
             with open(self.file_path, "w") as file:
-                file.write(self._html_content)
+                file.write(self._content)
         except IOError as e:
             raise IOError(f"Failed to write HTML file: {e}")
 
--- tests/test_report.py
+++ tests/test_report.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from unittest.mock import Mock, MagicMock
-from quantAnalytics.report import ReportGenerator, Header
+from quantAnalytics.report import ReportBuilder, DivBuilder, Header
 import shutil
 
 
@@ -55,20 +55,12 @@ class TestReportGenerator(unittest.TestCase):
         self.output_directory = "test_report"
         self.html_name = "test_report.html"
         self.pdf_name = self.html_name.replace(".html", ".pdf")
-        self.report = ReportGenerator(self.html_name, self.output_directory)
+        self.report = ReportBuilder(self.html_name, self.output_directory)
 
-    # @classmethod
     def tearDown(self):
         """Tear down test fixtures."""
         if os.path.exists(self.output_directory):
             shutil.rmtree(self.output_directory)
-        # html = os.path.join(cls.output_directory, cls.html_name)
-        # if os.path.exists(cls.output_directory):
-        #     os.rmdir(cls.output_directory)
-        #
-        # pdf = os.path.join(self.output_directory, self.pdf_name)
-        # if os.path.exists(pdf):
-        #     os.remove(pdf)
 
     def test_add_header(self):
         # Test
@@ -76,15 +68,17 @@ class TestReportGenerator(unittest.TestCase):
         self.report.add_header("Header Test", Header.H2)
 
         # Validate
-        self.assertIn('<h1 class="test">Header Test</h1>', self.report._html_content)
-        self.assertIn("<h2>Header Test</h2>", self.report._html_content)
+        report_content = self.report._content
+        self.assertIn('<h1 class="test">Header Test</h1>', report_content)
+        self.assertIn("<h2>Header Test</h2>", report_content)
 
     def test_add_paragraph(self):
         # Test
         self.report.add_paragraph("Hello this is a paragraph", "para")
 
         # Validate
-        self.assertIn('<p class="para">Hello this is a paragraph</p>', self.report._html_content)
+        report_content = self.report._content
+        self.assertIn('<p class="para">Hello this is a paragraph</p>', report_content)
 
     def test_add_unorderdlist_dict(self):
         # Test
@@ -95,13 +89,14 @@ class TestReportGenerator(unittest.TestCase):
         self.report.add_unorderedlist_dict(summary_dict, bold_key=False, css_class="dict2")
 
         # Validate
+        report_content = self.report._content
         self.assertIn(
             f"""<ul class="dict1">\n<li><strong>key1:</strong> value1</li>\n<li><strong>key2:</strong> value2</li>\n</ul>""",
-            self.report._html_content,
+            report_content,
         )
         self.assertIn(
             f"""<ul class="dict2">\n<li>key1: value1</li>\n<li>key2: value2</li>\n</ul>""",
-            self.report._html_content,
+            report_content,
         )
 
     def test_add_unorderedlist(self):
@@ -110,9 +105,10 @@ class TestReportGenerator(unittest.TestCase):
         self.report.add_unorderedlist(ordered_list, css_class="list1")
 
         # Validate
+        report_content = self.report._content
         self.assertIn(
             f"""<ul class="list1">\n<li>hello</li>\n<li>world</li>\n</ul>""",
-            self.report._html_content,
+            report_content,
         )
 
     def test_add_orderedlist(self):
@@ -121,9 +117,10 @@ class TestReportGenerator(unittest.TestCase):
         self.report.add_orderedlist(ordered_list, css_class="list2")
 
         # Validate
+        report_content = self.report._content
         self.assertIn(
             f"""<ol class="list2">\n<li>hello</li>\n<li>world</li>\n</ol>""",
-            self.report._html_content,
+            report_content,
         )
 
     def test_add_table(self):
@@ -132,9 +129,10 @@ class TestReportGenerator(unittest.TestCase):
         self.report.add_table(df, "Title", Header.H4, "title_css", "table_css")
 
         # Validate
+        report_content = self.report._content
         self.assertIn(
             f"""<h4 class=\'h4\'>Title</h4>\n<table class="table_css"><table border="1" class="dataframe">\n  <thead>\n    <tr style="text-align: right;">\n      <th>Column1</th>\n      <th>Column2</th>\n    </tr>\n  </thead>\n  <tbody>\n    <tr>\n      <td>1</td>\n      <td>3</td>\n    </tr>\n    <tr>\n      <td>2</td>\n      <td>4</td>\n    </tr>\n  </tbody>\n</table>\n</table>""",
-            self.report._html_content,
+            report_content,
         )
 
     def test_add_image(self):
@@ -144,21 +142,60 @@ class TestReportGenerator(unittest.TestCase):
         self.report.add_image("output_plot.png", css_class="image1")
 
         # Validate
-        self.assertIn(f"""<img src="output_plot.png" class="image1" alt="Plot Image"><br>""", self.report._html_content)
+        report_content = self.report._content
+        self.assertIn(f"""<img src="output_plot.png" class="image1" alt="Plot Image"><br>""", report_content)
 
     def test_complete_report(self):
         # Setup
         mock_html_content = "<html><body>Report content</body></html>"
-        self.report._html_content = mock_html_content
+        self.report._content = mock_html_content
         self.report._generate_html = Mock()
         self.report._generate_pdf = Mock()
 
         # test
-        self.report.complete_report()
+        self.report.build()
 
         # Validate HTML file write
         self.report._generate_html.assert_called_once()
         self.report._generate_pdf.assert_called_once()
+
+    def test_add_html_block(self):
+        html = report_content = "<h2>Header Test</h2>"
+        html += '<p class="para">Hello this is a paragraph</p>'
+
+        # Test
+        self.report.add_html_block(html)
+
+        # Validate
+        report_content = self.report._content
+        self.assertIn('\n<h2>Header Test</h2><p class="para">Hello this is a paragraph</p>', report_content)
+
+    def test_full_build(self):
+        # Test
+        ordered_list = ["hello", "world"]
+        summary_dict = {"key1": "value1", "key2": "value2"}
+
+        # fmt: off
+        self.report.add_header("Header1", Header.H1) \
+            .add_paragraph("Hello this is a paragraph", "para") \
+            .add_unorderedlist(ordered_list, css_class="list1") \
+            .add_unorderedlist_dict(summary_dict, True, "dict1") \
+            .add_div(DivBuilder().add_header("DivHeader", Header.H2, "div_header").build()) \
+            .build()
+        # fmt: on
+
+        # Validate
+        if os.path.exists(self.output_directory):  # Make sure you reference the correct file path
+            # Open and read the generated file
+            with open(self.report.file_path, "r") as f:
+                content = f.read()
+
+            # Optionally, validate content (simple check for specific elements)
+            assert "Header1" in content, "Header is missing in the generated file."
+            assert "Hello this is a paragraph" in content, "Paragraph is missing in the generated file."
+            assert '<ul class="list1">' in content, "Unordered list is missing in the generated file."
+            assert "<strong>key1:</strong>" in content, "Dictionary values are missing in the generated file."
+            assert '<h2 class="div_header">DivHeader</h2>' in content, "Div missing from file."
 
     # error handling
     def test_complete_report_errors(self):
@@ -168,7 +205,7 @@ class TestReportGenerator(unittest.TestCase):
 
         # Test
         with self.assertRaises(Exception) as context:
-            self.report.complete_report()
+            self.report.build()
 
         # Validate
         self.assertTrue("Error generating report" in str(context.exception))
